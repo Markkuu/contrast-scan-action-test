3c726a3b292c71742d2cf6ac861b8ddae8b7c75c
Mark Weeks <marrkkuu@gmail.com>
2022-05-11 16:00:10 +0100

    update param

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,8 @@
 #!/bin/sh -l
-echo "$@"
-
 echo "API Key: $INPUT_APIKEY"
 echo "Org ID: $INPUT_ORGID"
 echo "Project Name: $INPUT_PROJECTNAME"
+echo "Project ID: $INPUT_PROJECTID"
 echo "API URL $INPUT_APIURL"
 echo "Auth Header: $INPUT_AUTHHEADER"
 echo "Artifact: $INPUT_ARTIFACT"
@@ -13,4 +12,4 @@ echo "Timeout: $INPUT_TIMEOUT"
 
 echo "contrast-cli --api_key $INPUT_APIKEY --authorization $INPUT_AUTHHEADER --host $INPUT_APIURL --application_id "none" --organization_id $INPUT_ORGID --project_name $INPUT_PROJECTNAME --scan $INPUT_ARTIFACT --scan_timeout ${INPUT_TIMEOUT:-300} --language $INPUT_LANGUAGE --wait_for_scan --save_scan_results"
 
-contrast-cli --api_key "$INPUT_APIKEY" --authorization "$INPUT_AUTHHEADER" --host "$INPUT_APIURL" --application_id "none" --organization_id "$INPUT_ORGID" --project_name "$INPUT_PROJECTNAME" --scan "$INPUT_ARTIFACT" --scan_timeout "${INPUT_TIMEOUT:-300}" --language "$INPUT_LANGUAGE" --wait_for_scan --save_scan_results
+contrast-cli --api_key "$INPUT_APIKEY" --authorization "$INPUT_AUTHHEADER" --host "$INPUT_APIURL" --application_id "none" --organization_id "$INPUT_ORGID" --project_id "$INPUT_PROJECTID" --scan "$INPUT_ARTIFACT" --scan_timeout "${INPUT_TIMEOUT:-300}" --language "$INPUT_LANGUAGE" --wait_for_scan --save_scan_results
