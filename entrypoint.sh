2d004fd806c423a7c9ca5ce47d34a35a4da48c4e
Mark Weeks <marrkkuu@gmail.com>
2022-05-12 13:43:54 +0100

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,3 +10,18 @@ echo "Languages: $INPUT_LANGUAGE"
 echo "Timeout: $INPUT_TIMEOUT"
 
 contrast-cli --scan "$INPUT_ARTIFACT" --api_key "$INPUT_APIKEY"  --authorization "$INPUT_AUTHHEADER" --organization_id "$INPUT_ORGID" --host "$INPUT_APIURL" --project_name "$INPUT_PROJECTNAME" --language "$INPUT_LANGUAGE" --scan_timeout "${INPUT_TIMEOUT:-300}" --wait_for_scan --save_scan_results
+
+jq '.runs[].results | length' results.json
+
+file_size() {
+	file=$1
+	minimumsize=10485760
+	actualsize=$(wc -c <"$file")
+		if [ $actualsize -ge $minimumsize ]; then
+	    echo size is over $minimumsize bytes
+		else
+	    echo size is under $minimumsize bytes
+		fi
+}
+
+file_size results.json
